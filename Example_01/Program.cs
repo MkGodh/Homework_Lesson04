8d47ffc9fcda8f17fc4336157dd543ee01069aa7
MkGodh <fenryrvla@yandex.ru>
2023-12-18 21:25:20 +0300

    Initial commit

---
+++ Example_01/Program.cs
@@ -0,0 +1,89 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+using System;
+
+
+
+class Program
+
+{
+
+    static bool IsEvenSum(int num)
+
+    {
+
+        int sum = 0;
+
+        foreach (char c in num.ToString())
+
+        {
+
+            sum += int.Parse(c.ToString());
+
+        }
+
+        return sum % 2 == 0;
+
+    }
+
+
+
+    static void Main(string[] args)
+
+    {
+
+        string input;
+
+
+
+        while (true)
+
+        {
+
+            Console.Write("Введите целое число или 'q' для завершения программы: ");
+
+            input = Console.ReadLine();
+
+
+
+            if (input == "q")
+
+            {
+
+                break;
+
+            }
+
+
+
+            if (int.TryParse(input, out int num))
+
+            {
+
+                if (IsEvenSum(num))
+
+                {
+
+                    break;
+
+                }
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Введено некорректное значение, попробуйте снова");
+
+            }
+
+        }
+
+
+
+        Console.WriteLine("Программа завершена");
+
+    }
+
+}
---
+++ Example_03/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+ 
+public class Example
+{
+    public static void Main()
+    {
+        int[] array = { 1, 2, 3, 4 };
+        Array.Reverse(array);
+ 
+        Console.WriteLine(String.Join(',', array));
+    }
+}
+ 
